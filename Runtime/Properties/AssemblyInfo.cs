030e0257e9ef9de950cc53cbb863440c1ce3da91
Brian MacIntosh <brian@brianmacintosh.com>
2020-07-11 16:13:21 -0700

    metas

--- Runtime/Properties/AssemblyInfo.cs
+++ Runtime/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Runtime")]
+[assembly: AssemblyTitle("Dialogue Designer Unity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Brian MacIntosh")]
 [assembly: AssemblyProduct("Runtime")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Brian MacIntosh 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
